5a0c186dee1382c4d15bdca84187f0e99e5ee77b
James <jwlayug@gmail.com>
2025-10-26 15:35:10 +0800

    add charts

--- src/components/charts/TravelNetIncomePerDestinationChart.tsx
+++ src/components/charts/TravelNetIncomePerDestinationChart.tsx
@@ -38,6 +38,7 @@ export default function TravelNetIncomePerDestinationChart() {
         0
       );
 
+    //   const totalExpenses = groupWages + otherExpenses + driverWage;
       const totalExpenses = groupWages + otherExpenses;
       const netIncome = totalIncome - totalExpenses;
 
